e19895e7262dae00335e63b170a8ca79c4d312f3
guilhermealwayscommit <guilherme@neospace.ai>
2024-08-05 09:42:43 -0300

    Revert "feat: add status to tool message (optional parameter)"
    This reverts commit 711a0ac8ea409c1681b13aa8a85cb81570349b8a.

--- src/neospace/types/chat/chat_completion_tool_message_param.py
+++ src/neospace/types/chat/chat_completion_tool_message_param.py
@@ -16,10 +16,3 @@ class ChatCompletionToolMessageParam(TypedDict, total=False):
 
     tool_call_id: Required[str]
     """Tool call that this message is responding to."""
-
-    status: Literal["SUCCESS", "SERVER_ERROR", "CLIENT_ERROR"]
-    """
-    The status of the tool message. `SUCCESS` if the tool message was successful (200), 
-    `SERVER_ERROR` if the tool message was unsuccessful due to a server error (500), 
-    or `CLIENT_ERROR` if the tool message was unsuccessful due to a client error (400).
-    """
